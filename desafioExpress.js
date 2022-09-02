38b12f6c631895611631e49a91bf73660f6f5d24
ezequieleandi <eandiezequiel@gmail.com>
2022-09-02 20:33:52 -0300

    desafio express final

--- desafioExpress.js
+++ desafioExpress.js
@@ -2,16 +2,52 @@ const fs = require('fs')
 const express = require('express')
 const app = express()
 
-app.get('/', ((req, res) =>{
-    res.send('<h1 style="color:blue">bienvenido a la pagina</h1>')
-}))
+class Productos {
+    constructor(fileProducts) {
+        this.fileProducts = fileProducts
+    }
+    async save(product) {
+        const products = await this.getAll();
+        let newId;
+        products.length> 0 ? newId = products[products.length-1].id + 1 : newId = 1;
+        const newProd = { ...product, id: newId }
+        products.push(newProd);
+        try{
+            await fs.promises.writeFile(this.fileProducts, JSON.stringify(products))
+            return newId;
+        }catch(error){
+            throw new Error(`Error al guardar: ${error}`)
+        }
+    }
+    async getAll() {
+        try {
+            const products = await fs.promises.readFile(this.fileProducts, 'utf-8')
+            return JSON.parse(products)
+          } catch (error) {
+            return []
+          }
+    }
+    async randomProduct() {
+        const products = await this.getAll();
+        return products[Math.floor((Math.random() * products.length))]
+    }
+}
 
-app.get("/productos", (req, res) => {
-    res.send(producto1.getAll())
+const producto1 = new Productos('./productos.txt');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get('/', (req, res) =>{
+    res.send('<h1 style="color:blue">bienvenido a la pagina</h1>')
 })
 
-app.get("/productoRandom", (req, res) => {
-    res.send(producto1.randomProduct())
+app.get("/productos", async(req, res) => {
+    res.send(await producto1.getAll())
+})
+
+app.get("/productoRandom", async(req, res) => {
+    res.send(await producto1.randomProduct())
 })
 
 const PORT = 8080
@@ -19,37 +55,3 @@ const PORT = 8080
 app.listen(PORT, (req, res) => {
     console.log(`Escuchando al puerto ${PORT}`)
 })
-
-
-class Productos {
-    constructor(fileProducts) {
-        try {
-            this.fileProducts = fileProducts,
-            this.products = JSON.parse(fs.readFileSync(fileProducts, "utf-8"))
-            console.log(this.products)
-        } catch (e) {
-            fs.writeFileSync(fileProducts, JSON.stringify([]));
-            this.products = [];
-        }
-    }
-
-    save(product) {
-        let id = this.products.length> 0 ? this.products[this.products.length-1].id + 1 : 1;
-        product.id = id;
-        this.products.push(product);
-
-        return fs.promises.writeFile(this.fileProducts, JSON.stringify(this.products))
-        .then(() => {return id})
-        .catch((err) => { return err})
-    }
-
-    getAll() {
-        return this.products
-    }
-
-    randomProduct() {
-        return this.products[Math.floor((Math.random() * this.products.length))]
-    }
-}
-
-const producto1 = new Productos('./productos.txt');
